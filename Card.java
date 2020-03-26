76493c43bec35ba72db2e72d68bcc085dfc51668
Julia Cornell <cornelljuliah@gmail.com>
2020-03-26 13:25:46 -0400

    Added basic Card class

---
+++ Card.java
@@ -0,0 +1,29 @@
+public class Card {
+	Number number;
+	Color color;
+	
+	public Card(Color color, Number number) {
+		this.color = color;
+		this.number = number;
+	}
+
+	public Color getColor() {
+		return this.color;
+	}
+
+	public Number getNumber() {
+		return this.number;
+	}
+
+	public boolean isValidPlay(Card card) {
+		if (this.color == card.getColor()) {
+			return true;
+		} else if (this.color == color.WILD) {
+			return true;
+		} else if (this.number == card.getNumber()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
