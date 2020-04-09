cbcc0ea06d9fbb9d0a4fc774d5125486fe1684ed
tarabobinac <61031744+tarabobinac@users.noreply.github.com>
2020-04-09 13:07:41 -0400

    more efficient

--- Deck.java
+++ Deck.java
@@ -2,77 +2,68 @@ import java.util.*;
 
 public class Deck {
 
-	static ArrayList<Card> deck = new ArrayList<>();
-	static {
-		for (Color color : Color.normal_colors()) {
-			for (Number number : Number.normal_numbers()) {
-				if(number == Number.normal_numbers()[0]) {
-					deck.add(new Card(color, number));
-				} else {
-					deck.add(new Card(color, number));
-					deck.add(new Card(color, number));
-				}
-			}
-			for (Number number : Number.special_nonwild()) {
-				deck.add(new Card(color, number));
-				deck.add(new Card(color, number));
-			}
+        static Queue<Card> drawPile = new LinkedList<>();
+        static Stack<Card> discardPile = new Stack<>();
+        static ArrayList<Card> deck = new ArrayList<>();
 
-			deck.add(new Card(Color.WILD, Number.NONE));
-			deck.add(new Card(Color.WILD, Number.DRAW4));
-		}
-	}
-	
-	static Queue<Card> drawPile = new LinkedList<>();
-	static Stack<Card> discardPile = new Stack<>();
-	
+        //Constructor
+        public Deck() {
+                for (Color color : Color.normal_colors()) {
+                        for (Number number : Number.normal_numbers()) {
+                                if(number == Number.normal_numbers()[0]) {
+                                        this.deck.add(new Card(color, number));
+                                } else {
+                                        this.deck.add(new Card(color, number));
+                                        this.deck.add(new Card(color, number));
+                                }
+                        }
+                        for (Number number : Number.special_nonwild()) {
+                                this.deck.add(new Card(color, number));
+                                this.deck.add(new Card(color, number));
+                        }
 
-	//Constructor
-	public Deck() {
-		Random rand = new Random();
-		while(! deck.isEmpty()) {
-			drawPile.add(deck.get(rand.nextInt(deck.size())));
-		}
-	}
+                        this.deck.add(new Card(Color.WILD, Number.NONE));
+                        this.deck.add(new Card(Color.WILD, Number.DRAW4));
+                }
+        Collections.shuffle(this.deck);
+        while(!this.deck.isEmpty()) {
+                drawPile.add(deck.remove(0));
+                }
+        }
 
-	//DrawPile methods
-	
-	Random randomList = new Random();
-	private void shuffle() {
-		ArrayList<Card> copyDrawPile = new ArrayList<>();
-		while(drawPile.peek() != null) {
-			copyDrawPile.add(drawPile.remove());
-		}
-		while(! copyDrawPile.isEmpty()) {
-			drawPile.add(copyDrawPile.remove(randomList.nextInt(copyDrawPile.size())));
-		}
-	}
-		 
-	
-	public Card draw() {
-		if(drawPile.peek() == null) {
-			this.getNewDrawPile();
-		}
-		return drawPile.remove();
-	}
+        //DrawPile methods
+        private void shuffle() {
+                ArrayList<Card> copyDrawPile = new ArrayList<>();
+                while(drawPile.peek != null) {
+                        copyDrawPile.add(drawPile.remove());
+                }
+                Collections.shuffle(copyDrawPile);
+                while(!copyDrawPile.isEmpty()) {
+                        drawPile.add(copyDrawPile.remove(0));
+                }
+        }
 
-	//DiscardPile Methods
-	
-	private void getNewDrawPile() {	
-		Card top = discardPile.pop();
-		while (!discardPile.isEmpty()) {
-			drawPile.add(discardPile.pop());
-		}
-		discardPile.push(top);
-		this.shuffle();
-	}
+        public Card draw() {
+                if (drawPile.peek()==null) {
+                        this.getNewDrawPile();
+                }
+                return drawPile.remove();
+        }
 
-	public void discardCard(Card card) {
-		discardPile.push(card);
-	}
-	
-	public Card topDiscardCard() {
-		return discardPile.peek();
-	}
+        //DiscardPile methods
+        private void getNewDrawPile() {
+                Card top = discardPile.pop();
+                while (!discardPile.isEmpty()) {
+                        drawPile.add(discardPile.pop());
+                }
+                discardPile.push(top);
+                this.shuffle;
+        }
+        public void discardCard(Card card) {
+                discardPile.push(card);
+        }
+
+        public Card topDiscardCard() {
+                return discardPile.peek();
+        }
 }
-
