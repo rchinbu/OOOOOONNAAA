9b56740c9b614953df4cdf0a6c71199cd7faec9c
Maggie Zeng <maggiexzty@gmail.com>
2020-04-28 01:12:16 +0800

    merge

--- Color.java
+++
@@ -1,9 +0,0 @@
-public enum Color {
-	RED, YELLOW, BLUE, GREEN, WILD;
-
-	private static Color[] normal_colors_list = new Color[]{RED, YELLOW, BLUE, GREEN};
-
-	public static Color[] normal_colors() {
-		return normal_colors_list;
-	}
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# OOOOOONNAAA
-OONA is a game that has cards. It is similar to the game UNO but is not the same in that it is spelled “OONA” and is not copyrighted by Mattel
